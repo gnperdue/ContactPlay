ee9d235e1990b62e5cc54c0511fdde6f7b94c697
Gabriel Perdue <gnperdue@hey.com>
2021-02-06 09:39:01 -0600

    Initial Commit

---
+++ ContactPlay/ContactPlayApp.swift
@@ -0,0 +1,20 @@
+//
+//  ContactPlayApp.swift
+//  ContactPlay
+//
+//  Created by Gabriel Perdue on 2/6/21.
+//
+
+import SwiftUI
+
+@main
+struct ContactPlayApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}

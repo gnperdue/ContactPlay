5a02d13fe9eddfdc464f17e6c357bf740c28bf6d
Gabriel Perdue <gnperdue@hey.com>
2021-02-06 11:36:12 -0600

    add a stub for the simple sorted view

---
+++ ContactPlay/SimpleSortingChoiceView.swift
@@ -0,0 +1,20 @@
+//
+//  SimpleSortingChoiceView.swift
+//  ContactPlay
+//
+//  Created by Gabriel Perdue on 2/6/21.
+//
+
+import SwiftUI
+
+struct SimpleSortingChoiceView: View {
+  var body: some View {
+    Text("mellow")
+  }
+}
+
+struct SimpleSortingChoiceView_Previews: PreviewProvider {
+  static var previews: some View {
+    SimpleSortingChoiceView()
+  }
+}
